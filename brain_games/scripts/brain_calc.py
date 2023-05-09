ae26b3fae722e29aaf75a4adfa773bbf6b5ec6e8
Yelena Mikhailova <write1ikeme24@gmail.com>
2023-05-10 00:18:25 +0400

    second game brain-calc is ready

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from brain_games.games.calc_code import generate_questions
+from brain_games.cli import welcome_user
+
+def welcome():
+    print("Welcome to the Brain Games!")
+
+
+def main():
+    welcome()
+    name = welcome_user()
+
+    result = generate_questions()
+    if result == True:
+        print("Congratulations, " + name + "!")
+    else:
+        print("Let's try again, " + name + "!")
+
+        
+
+
+
+
+if __name__ == '__main__':
+    main()
